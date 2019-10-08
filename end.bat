4a319ff6f5381adbc121f358e88e11fb87ec3c21
Endika Vicioso <endika.vi@gmail.com>
2019-10-08 14:27:51 +0200

    mejoras en resolucion de fallos

--- end.bat
+++ end.bat
@@ -1,22 +1,41 @@
 @echo off
+
+see.bat | findstr /C:"%1"
+if %errorlevel% GEQ 1 GOTO:error1
+
 echo Subiendo Cambios de %1
-git push -u origin %1
-echo Subida Correcta
-if %ERRORLEVEL% GEQ 1 echo Error
 git checkout master
 echo Actualizando ultimos cambios desde Origen
 git pull
 echo Añadiendo cambios de %1 a master local
 git pull origin %1
 echo Subiendo Cambios
-git push origin master
+git push origin master | findstr /C:"failed"
+
+if %errorlevel% == 0 GOTO:resolve
+
+GOTO:end
+
+:resolve
+
+echo Se han encontrado cambios en master remoto
+git pull origin master
+git push origin master | findstr /C:"failed"
+
+if %errorlevel% == 0 GOTO:resolve
+
+GOTO:end
+
+:error1
+
+echo La rama especificada no existe
+GOTO:eof
+
+:end
+
 echo Borrando la Rama en local
 git branch -D %1
 echo Borrando la Rama en remoto
 git push origin :%1
 git checkout master
-if %ERRORLEVEL% GEQ 1 echo Error
-git pull origin master
-git push origin master
 echo Se ha finalizado la Rama y añadido a master correctamente
-if %ERRORLEVEL% GEQ 1 echo %ERRORLEVEL%
--- save.bat
+++ save.bat
@@ -4,5 +4,23 @@ git add *
 git status
 git commit -m %1
 echo Subiendo Cambios
-git push origin HEAD
+git push origin HEAD | findstr /C:"failed"
+
+if %errorlevel% == 0 GOTO:resolve
+
+GOTO:end
+
+:resolve
+
+echo Se han encontrado cambios en remoto
+
+git pull
+git push origin HEAD | findstr /C:"failed"
+
+if %errorlevel% == 0 GOTO:resolve
+
+GOTO:end
+
+:end
+
 echo Subida Correcta
